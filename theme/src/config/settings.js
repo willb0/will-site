cc957f167e631270b794eef9bf849c2d2479ef43
will bell <willbell0@icloud.com>
2023-07-06 12:41:21 -0700

    lazy commit lol

--- theme/src/config/settings.js
+++
@@ -1,9 +0,0 @@
-export default {
-	title: `Advisa`,
-	description: `Advisa`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Advisa`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: false,
-  showLittleSticksPlug: false,
-   // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
-};
