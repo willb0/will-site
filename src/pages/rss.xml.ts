18f38c9925677890163ef927dcf6e9923c5998ac
will bell <willbell0@icloud.com>
2023-07-06 12:38:31 -0700

    tweaked for advisa

--- src/pages/rss.xml.ts
+++
@@ -1,8 +0,0 @@
-import rss from '@astrojs/rss';
-
-export const get = () => rss({
-  title: 'Will Bell',
-  description: "will's website",
-  site: import.meta.env.SITE,
-  items: import.meta.glob('./blog/**.mdx'),
-});
